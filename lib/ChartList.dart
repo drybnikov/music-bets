dce528d189618b5e723d037920ae3efbea7a69f1
drybnikau <drybnikau@justaddmusic.net>
2020-03-05 22:48:58 +0100

    asyn loading

--- lib/ChartList.dart
+++ lib/ChartList.dart
@@ -2,6 +2,7 @@ import 'dart:convert' as convert;
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 import 'model/MediaItem.dart';
 
@@ -24,24 +25,51 @@ class ChartListHome extends StatefulWidget {
   _ChartListHome createState() {
     return _ChartListHome();
   }
-
-  Future loadChartList() async {
-    var url = 'https://www.googleapis.com/books/v1/volumes?q={http}';
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      final jsonResponse = convert.jsonDecode(response.body) as List;
-      List<MediaItemResponse> mediaList = jsonResponse.map((e) => new MediaItemResponse.fromJson(e)).toList();
-      print(mediaList);
-    }
-  }
 }
 
 class _ChartListHome extends State<ChartListHome> {
+  Future<List<MediaItemResponse>> futureChartList;
+
+  @override
+  void initState() {
+    super.initState();
+    futureChartList = fetchChartList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Weekly Chart')),
-      //body: _buildBody(context),
+      body: _buildBody(context),
     );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Center(
+      child: FutureBuilder<List<MediaItemResponse>>(
+        future: futureChartList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data.toString());
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
+
+Future<List<MediaItemResponse>> fetchChartList() async {
+  var url = 'https://api.jam-community.com/song/hottest/last7';
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    final jsonResponse = convert.jsonDecode(response.body) as List;
+    return jsonResponse.map((e) => new MediaItemResponse.fromJson(e)).toList();
+  } else {
+    throw Exception('Failed to load album');
   }
 }
--- lib/model/MediaItem.dart
+++ lib/model/MediaItem.dart
@@ -1,32 +1,31 @@
 class MediaItemResponse {
   final String id;
   final String name;
-  final String userId;
-  final String type;
+  final int plays;
+  final int likes;
+  final int comments;
   final String artistName;
   final String coverImage;
   final String filePath;
 
-
-  MediaItemResponse({
-    this.id,
-    this.name,
-    this.userId,
-    this.type,
-    this.artistName,
-    this.coverImage,
-    this.filePath
-  });
+  MediaItemResponse(
+      {this.id,
+      this.name,
+      this.plays,
+      this.likes,
+      this.comments,
+      this.artistName,
+      this.coverImage,
+      this.filePath});
 
   factory MediaItemResponse.fromJson(Map<String, dynamic> json) {
     return new MediaItemResponse(
         id: json['id'].toString(),
         name: json['name'],
-        userId: json['user_id'],
-        type: json['type'],
-        artistName: json['artist_name'],
+        plays: json['plays'],
+        likes: json['likes'],
+        comments: json['comments'],
         coverImage: json['cover_image_path'],
-        filePath: json['file_path']
-    );
+        filePath: json['music_file_path']);
   }
 }
